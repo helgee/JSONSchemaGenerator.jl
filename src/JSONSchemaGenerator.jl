1e6086d6d3a5203777bf18a427ede2720c3501d2
Helge Eichhorn <git@helgeeichhorn.de>
2021-04-15 00:55:51 +0200

    Initial experiment

--- src/JSONSchemaGenerator.jl
+++ src/JSONSchemaGenerator.jl
@@ -1,5 +1,113 @@
 module JSONSchemaGenerator
 
-# Write your package code here.
+using JSON3
+using OrderedCollections: OrderedDict
+using StructTypes
+
+export JSONType, JSONArray, Reference, Property, Object, Schema
+export generate, isstruct, isrequired, structtype, jsontype
+
+const SCHEMA_VERSION = "http://json-schema.org/draft-07/schema#"
+
+const PropType = OrderedDict{String, OrderedDict{String, String}}
+
+struct Reference
+	ref::String
+	Reference(name) = new("#/definitions/$name")
+end
+StructTypes.StructType(::Type{Reference}) = StructTypes.OrderedStruct()
+StructTypes.names(::Type{Reference}) = ((:ref, Symbol("\$ref")),)
+
+struct Property
+	type::String
+	enum::Array{String}
+	Property(type, enum=String[]) = new(type, enum)
+end
+StructTypes.StructType(::Type{Property}) = StructTypes.OrderedStruct()
+StructTypes.omitempties(::Type{Property}) = true
+
+struct Object
+	type::String
+	properties::OrderedDict{String, Union{Property, Reference}}
+	required::Array{String}
+	Object(properties, required) = new("object", properties, required)
+end
+StructTypes.StructType(::Type{Object}) = StructTypes.OrderedStruct()
+StructTypes.omitempties(::Type{Object}) = true
+
+struct JSONArray
+	type::String
+	items::Union{OrderedDict{String, String}, Reference}
+end
+
+jsonname(type) = lowercase(String(nameof(type)))
+
+isstruct(dt::DataType) = isstructtype(dt)
+isstruct(::Type{Union{T, Nothing}}) where {T} = isstructtype(T)
+structtype(dt::DataType) = dt
+structtype(::Type{Union{T, Nothing}}) where {T} = T
+isrequired(dt) = !(dt isa Union && Nothing <: dt)
+
+function define_structs!(definitions, type)
+	name = jsonname(type)
+	name in keys(definitions) && return
+
+	properties = OrderedDict{String, Union{Property, Reference}}()
+	required = String[]
+	for (fn, ftype) in zip(fieldnames(type), fieldtypes(type))
+		fname = String(fn)
+		if isrequired(ftype)
+			push!(required, fname)
+		end
+		if jsontype(ftype) == "object"
+			stype = structtype(ftype)
+			jtype = jsonname(stype)
+			# Recurse into undefined field types
+			jtype in keys(definitions) || define_structs!(definitions, ftype)
+			push!(properties, fname=>Reference(jtype))
+			continue
+		end
+		push!(properties, fname=>Property(jsontype(ftype)))
+	end
+	push!(definitions, name=>Object(properties, required))
+end
+
+struct Schema
+	schema::String
+	definitions::OrderedDict{String, Object}
+	type::String
+	properties::OrderedDict{String, Union{Property, Reference}}
+	function Schema(model::DataType)
+		definitions = OrderedDict{String, Object}()
+		type = "object"
+		properties = OrderedDict{String, Union{Property, Reference}}()
+		for (fn, ftype) in zip(fieldnames(model), fieldtypes(model))
+			fname = String(fn)
+			if jsontype(ftype) == "object"
+				stype = structtype(ftype)
+				jtype = jsonname(stype)
+				# Recurse into undefined field types
+				jtype in keys(definitions) || define_structs!(definitions, ftype)
+				push!(properties, fname=>Reference(jtype))
+				continue
+			end
+			push!(properties, fname=>Property(jsontype(ftype)))
+		end
+		return new(SCHEMA_VERSION, definitions, type, properties)
+	end
+end
+StructTypes.StructType(::Type{Schema}) = StructTypes.OrderedStruct()
+StructTypes.names(::Type{Schema}) = ((:schema, Symbol("\$schema")),)
+
+function generate(schema::Schema; pretty=false)
+	json = JSON3.write(schema)
+	pretty && return JSON3.pretty(json)
+	return json
+end
+
+jsontype(::Type{<:AbstractString}) = "string"
+jsontype(::Type{<:Integer}) = "integer"
+jsontype(::Type{<:AbstractFloat}) = "number"
+jsontype(::Type{T}) where {T} = "object"
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,53 @@
 using JSONSchemaGenerator
 using Test
 
+struct Address
+	street_address::String
+	city::String
+	state::String
+end
+
+struct Model
+    billing_address::Address
+    shipping_address::Address
+end
+
+const ADDRESS_EXAMPLE = """
+{
+  "\$schema": "http://json-schema.org/draft-07/schema#",
+
+  "definitions": {
+    "address": {
+      "type": "object",
+      "properties": {
+        "street_address": { "type": "string" },
+        "city":           { "type": "string" },
+        "state":          { "type": "string" }
+      },
+      "required": ["street_address", "city", "state"]
+    }
+  },
+
+  "type": "object",
+
+  "properties": {
+    "billing_address": { "\$ref": "#/definitions/address" },
+    "shipping_address": { "\$ref": "#/definitions/address" }
+  }
+}
+"""
+
 @testset "JSONSchemaGenerator.jl" begin
-    # Write your tests here.
+    @test jsontype(Int) == "integer"
+    @test jsontype(String) == "string"
+    @test jsontype(Float64) == "number"
+    @test jsontype(Address) == "object"
+    @test isstruct(Address)
+    @test isstruct(Union{Nothing, Address})
+    @test structtype(Address) == Address
+    @test structtype(Union{Nothing, Address}) == Address
+    @test isrequired(Address)
+    @test !isrequired(Union{Nothing, Address})
+    schema = Schema(Model)
+    @test generate(schema) == replace(ADDRESS_EXAMPLE, r"\s"=>"")
 end
